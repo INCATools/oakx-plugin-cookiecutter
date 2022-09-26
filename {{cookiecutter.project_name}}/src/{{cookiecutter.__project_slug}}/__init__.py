79f1435f813e0caec3e959bfae47f94e88eaf5a1
Patrick Kalita <pkalita@lbl.gov>
2022-09-26 15:42:10 -0700

    Register plugin via oaklib.plugins entrypoint
    This also increases the minimum python version of the generated project to 3.9 in order to be able to get the latest versions of oaklib (which require python 3.9) to be installed.

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
@@ -2,9 +2,3 @@
 from importlib import metadata
 
 __version__ = metadata.version(__name__)
-
-from {{cookiecutter.__project_slug}} import {{cookiecutter.implementation}}
-
-schemes = {
-    '{{cookiecutter.scheme}}': {{cookiecutter.implementation}}
-}
