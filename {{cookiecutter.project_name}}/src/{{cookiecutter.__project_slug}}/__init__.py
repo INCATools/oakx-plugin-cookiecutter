497ef127e6277ca7598922638215974c8989bd3a
Harshad Hegde <hegdehb@gmail.com>
2022-10-28 16:54:18 -0500

    Dynamic versioning

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/__init__.py
@@ -1,4 +1,12 @@
 """{{cookiecutter.__project_slug}} package."""
-from importlib import metadata
-
-__version__ = metadata.version(__name__)
+try:
+    from importlib import metadata
+except ImportError:
+    # Python < 3.8
+    import importlib_metadata as metadata  # pragma: no cover
+                
+try:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"  # pragma: no cover
